35526e57059703d85a460759a4efb826f79537ea
Kian Boon Koh <kianboonkoh@gmail.com>
2019-06-18 18:27:22 +0800

    implement command line argument parsing

--- neural_transfer.py
+++ neural_transfer.py
@@ -1,11 +1,19 @@
 import tensorflow as tf
 from pathlib import Path
 import time
+import argparse
 
 from src.utils import load_image, save_image
 from src.model import VGG19Model
 
+# for demo purposes
+DEMO_CONTENT = tf.keras.utils.get_file(
+    'turtle.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/Green_Sea_Turtle_grazing_seagrass.jpg')
+DEMO_STYLE = tf.keras.utils.get_file(
+    'kandinsky.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+
 DATA_PATH = Path('./data/')
+
 # Content layer where we will pull our feature maps
 CONTENT_LAYERS = ['block5_conv2']
 
@@ -16,11 +24,34 @@ STYLE_LAYERS = ['block1_conv1',
                 'block4_conv1',
                 'block5_conv1']
 
+parser = argparse.ArgumentParser(description='Perform neural style transfer.')
+parser.add_argument('--content-path', type=str, default=DEMO_CONTENT,
+                    dest='content_path', help='path of content image')
+parser.add_argument('--style-path', type=str, default=DEMO_STYLE,
+                    dest='style_path', help='path of style image')
+parser.add_argument('--style-weight', type=float, default=1.0,
+                    dest='style_weight', help='style weight')
+parser.add_argument('--content-weight', type=float, default=0.4,
+                    dest='content_weight', help='content weight')
+parser.add_argument('--variation-weight', type=float, default=2e4,
+                    dest='variation_weight', help='variation weight')
+parser.add_argument('-lr', '--learning-rate', type=float, default=10.0,
+                    dest='learning_rate', help='variation weight')
+parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
+parser.add_argument('--steps', type=int, default=100,
+                    dest='steps_per_epoch', help='number of steps per epoch')
 
-content_path = tf.keras.utils.get_file(
-    'turtle.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/Green_Sea_Turtle_grazing_seagrass.jpg')
-style_path = tf.keras.utils.get_file(
-    'kandinsky.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+args = parser.parse_args()
+
+print('Running neural style tranfer with the following parameters:')
+print()
+
+for key, value in vars(args).items():
+    print(f'\t{key}: {value}')
+print()
+
+content_path = args.content_path
+style_path = args.style_path
 
 content_image, style_image = [load_image(
     path) for path in (content_path, style_path)]
@@ -31,17 +62,20 @@ image = tf.Variable(content_image)
 style_targets = style_content_model(style_image)['style_outputs']
 content_targets = style_content_model(content_image)['content_outputs']
 
-style_weight = 1
-content_weight = 0.4
-variation_weight = 2e4
+style_weight = args.style_weight
+content_weight = args.content_weight
+variation_weight = args.variation_weight
 
-opt = tf.keras.optimizers.Adam(learning_rate=10, beta_1=0.99, epsilon=1e-1)
+learning_rate = args.learning_rate
+opt = tf.keras.optimizers.Adam(
+    learning_rate=learning_rate, beta_1=0.99, epsilon=1e-1)
+
 style_content_model.compile(opt)
 
 start = time.time()
 
-epochs = 10
-steps_per_epoch = 100
+epochs = args.epochs
+steps_per_epoch = args.steps_per_epoch
 
 for n in range(epochs*steps_per_epoch):
     style_content_model.fit(image,
